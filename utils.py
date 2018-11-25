efa203ba5cbdee32d125891eb58476edab6394ae
Corey Zheng <czheng45@gatech.edu>
2018-11-25 17:03:47 -0500

    Fixing glob directory string issue causing images to not be read.

--- utils.py
+++ utils.py
@@ -103,7 +103,7 @@ def getImageStack(directory):
     # imageStack = a size N list that contains all the jpg images contained within the folder
     #
     #
-    imnames = glob.glob(directory + '\*.jpg')  # get all calibration image files
+    imnames = glob.glob(directory + '/*.jpg')  # get all calibration image files
     imnames.sort()  # sort into numerical order
 
     imageStack = []
