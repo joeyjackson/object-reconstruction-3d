ff110d2ada4b62182c26833a254f76ae5ad8422f
Corey Zheng <czheng45@gatech.edu>
2018-11-25 16:55:26 -0500

    Added comments and revised functionality of image taker

--- calibrateCamera.py
+++ calibrateCamera.py
@@ -2,20 +2,31 @@ import numpy as np
 import cv2
 import glob
 
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-cal_imgs = glob.glob("cal\*.jpg")  # get all calibration image files
-cal_imgs.sort()  # sort into numerical order
-cal_dim = (9, 6)  # dimensions of desired calibration grid in points, on an arbitrary axis
+# USER PARAMETERS
+calibration_directory = 'cal'  # folder where all the calibration images are stored
 scale = 12  # scale of the calibration coordinate system in mm
 dtheta = 10  # rotation of every image used for calibration
+cal_dim = (9, 6)  # dimensions of desired calibration grid in points, on an arbitrary axis
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)  # calibration criteria
+
+# Parameters setup
+xoffset = scale*(cal_dim[0] - 1)/2  # offset of the corner point to the rotation center, x axis
+yoffset = scale*(cal_dim[1] - 1)/2  # offset of the corner point to the rotation center, y axis
+
+cal_imgs = glob.glob(calibration_directory + '\*.jpg')  # get all calibration image files
+cal_imgs.sort()  # sort into numerical order
+
 d3_points = []  # the true 3D coordinates in each calibration image. Holds copies of calp
 d2_points = []  # the resulting 2D pixel coordinates of each calibration point.
 
-ax1 = scale * np.tile(np.arange(0, cal_dim[0])[:, np.newaxis], (cal_dim[1],1)) - 48 # create 'x' coordinates
-ax2 = scale * np.repeat(np.arange(0, cal_dim[1]), cal_dim[0])[:, np.newaxis] - 30 # create 'y' coordinates
-ax3 = np.zeros((cal_dim[0]*cal_dim[1],1)) # create 'z' coordinates
+# 3D Coordinate grid creation
+ax1 = scale * np.tile(np.arange(0, cal_dim[0])[:, np.newaxis], (cal_dim[1],1)) - xoffset # create 'x' coordinates
+ax2 = scale * np.repeat(np.arange(0, cal_dim[1]), cal_dim[0])[:, np.newaxis] - yoffset  # create 'y' coordinates
+ax3 = np.zeros((cal_dim[0]*cal_dim[1],1))  # create 'z' coordinates
+
 calp = np.float32(np.concatenate([ax1, ax2, ax3], 1))  # get the points the calibration image holds
 
+# Calibration loop
 angles = []
 for imgname in cal_imgs:
     imcol = cv2.imread(imgname)
@@ -23,7 +34,7 @@ for imgname in cal_imgs:
     r, corners = cv2.findChessboardCorners(img, cal_dim, None)  # find calibration corners
 
     if r == True:  # we found corners successfully
-        angles.append(10*(int(imgname[10:-4]) - 1))  # record the angle the shot was taken at for housekeeping
+        angles.append(10*(int(imgname[-6:-4]) - 1))  # record the angle the shot was taken at for housekeeping
         cornerpts = cv2.cornerSubPix(img, corners, (11, 11), (-1, -1), criteria)  # refine calibration corners
 
         d2_points.append(cornerpts)  # where do the chessboard points lie in the 2D image?
---
+++ measurement.py
@@ -0,0 +1,37 @@
+import cv2
+import utils
+
+# Name of the images to save as
+samplename = 'rubiks/rubiks'
+
+# Begin video capture
+cv2.namedWindow("Press ENTER to take measurement, ESC to exit.\n" +
+                "Rotate the turntable by a fixed angle for each  capture.")
+vc = cv2.VideoCapture(0)
+vc.set(3, 1920)
+vc.set(4, 1080)
+
+if vc.isOpened():
+    ret, img = vc.read()
+else:
+    ret = False
+
+# Begin save loop
+sample = 1
+while ret:
+
+    cv2.imshow("Press ENTER to take measurement, ESC to exit.\n" +
+               "Rotate the turntable by a fixed angle for each capture.",
+               cv2.resize(img, (int(img.shape[1]/2), int(img.shape[0]/2)) ) )  # show camera feed scaled by half
+    ret, img = vc.read()
+    img = utils.flipImage(img)
+
+    key = cv2.waitKey(20)
+    if key == 13:  # user hit enter
+        rval = cv2.imwrite(samplename+"%02d"%sample+".jpg", img)
+        print("Took measurement " + str(sample))
+        sample += 1
+    if key == 27:  # user hit esc
+        break
+
+cv2.destroyWindow("Preview")
